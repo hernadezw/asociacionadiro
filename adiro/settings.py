6783d6f87616f5e3e27fff79a789fae046209807
hernadezw <lhernandez1203@gmail.com>
2025-02-07 15:57:29 -0600

    segundo commit

--- adiro/settings.py
+++ adiro/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0*(x_o2uf#(x$^np=!j_qh8)ey5t4x+u#-w9xz_c0h($!8m1@v'
-
+#SECRET_KEY = 'django-insecure-0*(x_o2uf#(x$^np=!j_qh8)ey5t4x+u#-w9xz_c0h($!8m1@v'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -78,10 +80,21 @@ WSGI_APPLICATION = 'adiro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'adiro',
+        'USER': 'adiro',
+        'PASSWORD': 'administracion-2025-Adiro',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
